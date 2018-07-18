5270d8ac1fbf0d324686e73bcd90bb402136ac67
Nandan <nandan0297@gmail.com>
2018-07-19 00:33:58 +0530

    First Commit

---
+++ REAMDE.md
@@ -0,0 +1,2 @@
+#Photography Website
+This project is to demonstrate minor use of JQuery and major use of CSS to make a portfolio website for a photographer.
---
+++ js/main.js
@@ -0,0 +1,7 @@
+//animate smooth scroll
+
+$('#view-work').on('click',function(){
+  const images=$('#images').position().top;
+
+  $('html,body').animate({scrollTop: images},1500);
+});
